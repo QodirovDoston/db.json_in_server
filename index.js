17371ca8ab08c63fb6b03e73a34ff047cfc71427
QodirovDoston <qodirovdoston806@gmail.com>
2023-03-13 23:20:43 +0500

    fdfedf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonSever = require("json-server");
 const server = jsonSever.create();
 const router = jsonSever.router("db.json");
 const middlewares = jsonSever.defaults();
-const port = process.env.PORT || 1020;
+const port = process.env.PORT || 1030;
 
 server.use(middlewares);
 server.use(router);
